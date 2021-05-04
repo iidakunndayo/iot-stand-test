84e45caa5f0686d269d92541efb3d4f23facbe92
iidakunndayo <iidakunndayo@gmail.com>
2021-05-04 18:47:02 +0900

    動作試験用テストコードファイルの作成

---
+++ NewBoard/SingleCotrol/test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.7
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
---
+++ OldBoard/SingleCotrol/test.cpp
@@ -0,0 +1,9 @@
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+#define HIGH 1
+#define LOW 0
+
+int main() {
+
+}
